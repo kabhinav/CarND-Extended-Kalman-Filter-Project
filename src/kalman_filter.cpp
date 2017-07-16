625e03a4589e6ba437b86654850dba2d048c7fca
Navsha Computing <contact@navshacomputing.com>
2017-07-16 15:48:12 -0400

    Validated test against dataset 1.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,7 @@
 #include "kalman_filter.h"
+#include "cmath"
+
+#define PI 3.1415926535
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,22 +21,57 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  // map cartesian to polar coordinates
+  MatrixXd h_x(3,1);
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  float pxy = fmax(pow(px,2)+pow(py,2), 0.000001);
+  float pxy_sq = sqrt(pxy);
+
+   h_x << pxy_sq,
+          atan2(py, px),
+          (px*vx + py*vy)/pxy_sq;
+
+  VectorXd y = z - h_x;
+
+  // check angle is between -PI and PI
+  while(y(1) > PI) y(1) -= 2*PI;
+  while(y(1) < -PI) y(1) += 2*PI;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
